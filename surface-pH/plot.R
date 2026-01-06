938e1ffb3250576f96d682b50bcb2e759795b75a
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2026-01-06 18:22:15 +0100

    working on surface pH data

--- surface-pH/plot.R
+++ surface-pH/plot.R
@@ -1,6 +1,11 @@
 
 
-ggplot(df.summ[df.summ$cover == 'covered', ], aes(elapsed.time, pH.mn, color = treat)) + 
-  geom_point() + 
+df.summ1 <- df.summ[! df.summ$treat == 'Solid fraction', ]
+df.summ1 <- df.summ[! df.summ$treat == 'Solid fraction with biochar (high)', ]
+
+ggplot(df.summ[df.summ$cover == 'covered', ], aes(elapsed.time, pH.mn, color = treat, fill = treat)) + 
+  geom_point(shape = 1, size = 0.5) + 
   geom_line() + 
-  facet_wrap(~ new.ID, scales = 'free_x')
+  facet_wrap(~ new.ID, ncol = 3, scales = 'free_y') +
+  theme_bw() + 
+  geom_ribbon(aes (ymax = pH.mn + pH.sd, ymin = pH.mn - pH.sd, group = treat), alpha = 0.1, color = NA)
