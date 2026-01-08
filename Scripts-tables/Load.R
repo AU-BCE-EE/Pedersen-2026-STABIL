5ec59ebeca957956a68016723a5ccf3a3c94055d
Flavia Dela Pierre <flavia.delapierre@unito.it>
2026-01-08 10:43:22 +0100

    Update file paths for Excel data imports

--- Scripts-tables/Load.R
+++ Scripts-tables/Load.R
@@ -1,9 +1,10 @@
 
 # Read Excel file, skip first row
 
-data_slurry <- read_excel("../data/slurry properties.xlsx", 
+data_slurry <- read_excel("../data-slurry/slurry properties.xlsx", 
                    skip = 1)
-data_soil <- read_excel("../data/Soil properties.xlsx", 
+data_soil <- read_excel("../data-soil/Soil properties.xlsx", 
                    skip = 1)
+
 
 
