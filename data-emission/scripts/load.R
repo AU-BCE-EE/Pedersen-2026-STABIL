5a387be4de5a71be03ac0567f12f531415715300
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2026-01-13 21:42:40 +0100

    Update load.R

--- data-emission/scripts/load.R
+++ data-emission/scripts/load.R
@@ -3,5 +3,9 @@
 
 idat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/ffc0aae283f775e278924e37338324d34d29c7f2/data-output/03/ALFAM2_interval.csv.gz')
 
-pdat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/ffc0aae283f775e278924e37338324d34d29c7f2/data-output/03/ALFAM2_plot.csv.gz')
+pdat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/8073f961ce3bd716981430d37251de0d4349a5dc/data-output/03/ALFAM2_plot.csv.gz')
+
+pdat.new <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/ffc0aae283f775e278924e37338324d34d29c7f2/data-output/03/ALFAM2_plot.csv.gz')
+
+
 
