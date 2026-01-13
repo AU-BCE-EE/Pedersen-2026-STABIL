d2cf7dfd90beaae1ae34ef9e535a2d78cbc94da1
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2026-01-13 21:30:29 +0100

    updating url for loading data from ALFAM2 database

--- data-emission/scripts/load.R
+++ data-emission/scripts/load.R
@@ -1,6 +1,7 @@
 
 # Data not yet released, reading from the following commit: 
 
-idat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/8073f961ce3bd716981430d37251de0d4349a5dc/data-output/03/ALFAM2_interval.csv.gz')
+idat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/ffc0aae283f775e278924e37338324d34d29c7f2/data-output/03/ALFAM2_interval.csv.gz')
 
-pdat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/8073f961ce3bd716981430d37251de0d4349a5dc/data-output/03/ALFAM2_plot.csv.gz')
+pdat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/ffc0aae283f775e278924e37338324d34d29c7f2/data-output/03/ALFAM2_plot.csv.gz')
+
