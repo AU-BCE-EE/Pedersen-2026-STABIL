c556e89a3be582090001784f0bc65bdcb2e2abe0
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2026-01-14 19:28:05 +0100

    Update load.R

--- data-emission/scripts/load.R
+++ data-emission/scripts/load.R
@@ -7,5 +7,7 @@ pdat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/8073f961c
 
 pdat.new <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/ffc0aae283f775e278924e37338324d34d29c7f2/data-output/03/ALFAM2_plot.csv.gz')
 
+# checking if pdat.new (latest ALFAM2 database commit) has STABIL data
+test <- subset(pdat.new, proj == 'STABIL26')
 
 
