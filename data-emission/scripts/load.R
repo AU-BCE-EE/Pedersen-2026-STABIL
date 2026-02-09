21cc18b79f5a336db9bbec90cb249521e69fe8ed
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2026-02-09 17:37:46 +0100

    updating ALFAM2 database data source and temperature plot

--- data-emission/scripts/load.R
+++ data-emission/scripts/load.R
@@ -1,8 +1,8 @@
 
 # Data not yet released, reading from the following commit: 
 
-idat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/cd6743331e2f43fa0533b7a711f6ad6b4fbb699c/data-output/03/ALFAM2_interval.csv.gz')
+idat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/af0654817f633833c8bd52d407e2cb7dceb1e5a7/data-output/03/ALFAM2_interval.csv.gz')
 
-pdat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/cd6743331e2f43fa0533b7a711f6ad6b4fbb699c/data-output/03/ALFAM2_plot.csv.gz')
+pdat <- fread('https://raw.githubusercontent.com/AU-BCE-EE/ALFAM2-data/af0654817f633833c8bd52d407e2cb7dceb1e5a7/data-output/03/ALFAM2_plot.csv.gz')
 
 
